c378b1345914b42bc47ec195bf3409a3baf6448b
Tsuji-tomonori33 <tsuji-tomonori@kago.ed.jp>
2025-11-05 12:26:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const stampIds = ["stamp1", "stamp2", "stamp3", "stamp4", "stamp5"];
 
-const stampPositions = {
+const stampPositions = 
+{
+  "stamp1": {
+    "left": 12.833333333333332,
+    "top": 20.65982458726415
+  },
+  "stamp2": {
+    "left": 66.16666666666666,
+    "top": 24.6692585495283
+  },
+  "stamp3": {
+    "left": 27.333333333333332,
+    "top": 43.01849941037736
+  },
+  "stamp4": {
+    "left": 66.5,
+    "top": 54.339254127358494
+  },
+  "stamp5": {
+    "left": 15,
+    "top": 75.89548938679245
+  }
   "stamp1": { "left": 11.5, "top": 19.95227741745283 },
   "stamp2": { "left": 64.66666666666666, "top": 23.843786851415093 },
   "stamp3": { "left": 26, "top": 42.31095224056604 },
